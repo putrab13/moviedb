82640ed5365ecf7efd1105bf6db22b469926ad16
putrab13 <putra.b13@gmail.com>
2020-03-23 18:11:15 +0700

    perubahan api

--- conf/info.php
+++ conf/info.php
@@ -1,12 +1,7 @@
 <?php
 
-$apikey = "bb78e4cf3442e302d928f2c5edcdbee1";
-//$apikey = "834757820eeecce77f54425f4f7e6df1";
-//$apikey = "API key masing masing";
-//$sitename = "Update Film";
-//$tagline = "Info Film Update";
-//$email = "apajahboleh@gmail.com";
-//$url = "http://apajahboleh.net";
+$apikey = "isi API kalian masing masing";
+
 
 $imgurl_1 = "http://image.tmdb.org/t/p/w500";
 $imgurl_2 = "http://image.tmdb.org/t/p/w300";
